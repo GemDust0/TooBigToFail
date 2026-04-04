e22a9d53127e77a565a31a45ede02e503d28b11d
GemDust <a.bant@bant-web.nl>
2026-04-04 15:29:18 -0400

    Corporate Sequence cont.

--- EmployeeSequence.gd
+++ EmployeeSequence.gd
@@ -31,7 +31,7 @@ func _on_submit_pressed() -> void:
 		code_index += 1
 	else:
 		print(get_final_score())
-		get_tree().change_scene_to_file("res://Scenes/TitleScreen.tscn")
+		get_tree().change_scene_to_file("res://Scenes/CorporateSim.tscn")
 
 func get_final_score() -> float:
 	var finalScore: float = 0

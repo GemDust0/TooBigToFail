786ff631c81273a7371a755ce1d4c2e9906a83d0
GemDust <a.bant@bant-web.nl>
2026-04-04 17:00:46 -0400

    Temp. removal of synergies

--- Scripts/Employee.gd
+++ Scripts/Employee.gd
@@ -1,7 +1,11 @@
 class_name Employee extends Button
 
-@export var synergies: Array[Synergy]
+enum Types {
+	DEVELOPER
+}
+
 ## Money produced every production
 @export var production_value: int = 50
 ## Time to produce
 @export var production_time: float = 5.0
+@export var description: String = ""

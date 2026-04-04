a5626c5c88a1a2cbecd48a2bc18325192b87c67b
GemDust <a.bant@bant-web.nl>
2026-04-03 20:03:09 -0400

    Quit Game

--- Scripts/TitleScreen.gd
+++ Scripts/TitleScreen.gd
@@ -3,3 +3,7 @@ extends Node
 
 func _on_new_game_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/EmployeeSequence.tscn")
+
+
+func _on_exit_game_pressed() -> void:
+	get_tree().quit()

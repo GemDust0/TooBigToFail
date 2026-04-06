02ec107b43fcce5813bb395d0cf721250823b96a
GemDust <a.bant@bant-web.nl>
2026-04-06 16:01:13 -0400

    Relic Base

---
+++ Scripts/Relic.gd
@@ -0,0 +1,4 @@
+class_name Relic extends TextureRect
+
+@export var id: String
+@export var description: String
--- Scripts/RelicInventory.gd
+++ Scripts/RelicInventory.gd
@@ -1,1 +1,13 @@
 class_name RelicInventory extends TileMapLayer
+
+var relics: Array[Relic] = []
+
+func has_relic(id: String) -> bool:
+	for relic: Relic in relics:
+		if relic.id == id:
+			return true
+	return false
+
+func add_relic(relic: Relic) -> void:
+	relics.append(relic)
+	add_child(relic)

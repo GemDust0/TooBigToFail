83f3ebf332c398eae121783ffcf625ec4a7e401d
GemDust <a.bant@bant-web.nl>
2026-04-07 17:00:07 -0400

    Cutscene changes

--- Scripts/TransitionObject.gd
+++ Scripts/TransitionObject.gd
@@ -1,6 +1,7 @@
 class_name TransitionObject extends ColorRect
 
 @export var should_fade_in: bool = true
+@export var fade_in_delay: float = 0.5
 
 @onready var animation_player: AnimationPlayer = $Fade
 
@@ -20,6 +21,8 @@ func change_scene(scene: PackedScene) -> void:
 func fade_in() -> void:
 	get_tree().paused = true
 	mouse_filter = Control.MOUSE_FILTER_STOP
+	color.a = 1.0
+	await get_tree().create_timer(fade_in_delay).timeout
 	animation_player.play("fade_in")
 	await animation_player.animation_finished
 	get_tree().paused = false

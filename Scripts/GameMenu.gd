d526e39a1195879b5bd15430f7fd11c3a9b17a20
GemDust <a.bant@bant-web.nl>
2026-04-08 15:42:09 -0400

    MenuUpdate

--- Scripts/GameMenu.gd
+++ Scripts/GameMenu.gd
@@ -1,9 +1,9 @@
 class_name GameMenu extends Control
 
-@onready var menu: MarginContainer = $MenuContainer
+@onready var menu: ColorRect = $MenuGrayout
 
 func _ready() -> void:
-	$MenuContainer/MarginContainer/MarginContainer/VBoxContainer/HBoxContainer/SpinBox.value = AudioPlayer.volume_linear / AudioPlayer.max_volume * 100
+	$MenuGrayout/MenuContainer/MarginContainer/MarginContainer/VBoxContainer/HBoxContainer/SpinBox.value = AudioPlayer.volume_linear / AudioPlayer.max_volume * 100
 
 func open() -> void:
 	menu.visible = true

0f558b88953759181fcb6aedb88cddc8f9df08c9
GemDust <a.bant@bant-web.nl>
2026-04-13 19:48:29 -0400

    Added new relics to relic lookup

--- Scripts/Relic.gd
+++ Scripts/Relic.gd
@@ -1,15 +1,21 @@
 class_name Relic extends TextureRect
 
 const lookup: Dictionary[String, String] = {
+	"Added A Tutorial":"res://Scenes/Relics/AddedATutorial.tscn",
 	"Amateur Hour":"res://Scenes/Relics/AmateurHour.tscn",
+	"Analysis Squared":"res://Scenes/Relics/AmateurHour.tscn",
 	"Cats And Dogs":"res://Scenes/Relics/CatsAndDogs.tscn",
 	"Government Subsidies":"res://Scenes/Relics/GovernmentSubsidies.tscn",
 	"Intern Together Strong":"res://Scenes/Relics/InternTogetherStrong.tscn",
 	"Management Overhaul":"res://Scenes/Relics/ManagementOverhaul.tscn",
+	"Mentorship":"res://Scenes/Relics/AmateurHour.tscn",
 	"Mutualism":"res://Scenes/Relics/Mutualism.tscn",
+	"Proper Documentation":"res://Scenes/Relics/AmateurHour.tscn",
+	"Remaster In Rust":"res://Scenes/Relics/AmateurHour.tscn",
 	"Rubber Ducking":"res://Scenes/Relics/RubberDucking.tscn",
 	"Snitches get... Snitches??":"res://Scenes/Relics/SnitchesSnitches.tscn",
 	"Employee Stability":"res://Scenes/Relics/Stability.tscn",
+	"Support Circle":"res://Scenes/Relics/AmateurHour.tscn",
 	"Turning Losses (Around)":"res://Scenes/Relics/TurningLosses.tscn"
 }
 
